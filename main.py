59d2733f34f68701db0340a2db5cc1c17c92c468
MeEtSoNi-lab <meet@tulong.tech>
2025-07-31 21:06:53 -0400

    Initial 3 Commit

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
-import re
-
 app = FastAPI()
-
-# Request schema
 class EmailRequest(BaseModel):
     email: EmailStr
-
-# Email validation logic
-def is_disposable(email):
-    disposable_domains = ['mailinator.com', 'tempmail.com', '10minutemail.com']
-    domain = email.split('@')[1]
-    return domain in disposable_domains
-
 @app.post("/validate_email")
 async def validate_email(data: EmailRequest):
-    if is_disposable(data.email):
-        return {"status": "Disposable", "email": data.email}
-    return {"status": "Valid", "email": data.email}
+    return {"message": f"Email is: {data.email}"}
